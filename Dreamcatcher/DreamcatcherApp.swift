1c65f09aa3c20c38c1fb3692fbc6b8a7a156a779
seahpark247 <famousjedo@gmail.com>
2025-03-08 12:01:42 -0600

    Initial Commit

---
+++ Dreamcatcher/DreamcatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamcatcherApp.swift
+//  Dreamcatcher
+//
+//  Created by Seah Park on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamcatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DreamcatcherTests/DreamcatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  DreamcatcherTests.swift
+//  DreamcatcherTests
+//
+//  Created by Seah Park on 3/8/25.
+//
+
+import Testing
+@testable import Dreamcatcher
+
+struct DreamcatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
